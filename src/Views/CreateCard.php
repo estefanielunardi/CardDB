921fe22f68afa32efee36180a488051f2160ab1e
G4BR1TRZ <67324204+G4BR1TRZ@users.noreply.github.com>
2020-11-05 14:48:24 +0100

    CreatedCard Style
    styles of createdCard

--- src/Views/CreateCard.php
+++ src/Views/CreateCard.php
@@ -3,25 +3,26 @@
 <body>
     <?php require_once("Components/Header.php"); ?>
 
+    <main class="container text-center">
         <ul>
             <li  class="appointButton"><a class="appoint" href="index.php">Cards</a></li>
         </ul>
-    <main class="container text-center">
-        <h2>New Card</h2>
+    <div class="cardContainer">
         <div class="cardStyle">
             <form action='?action=store' method="post">
                 <div>
-                    <input placeholder="Enquiry Title" class="card-title" type="text" name="title">
-                    <input placeholder="Coder Name"class="card-subtitle mb-2 text-muted" type="text" name="name">
-                    <div class="btn-group" role="group" aria-label="Basic example">
+                    <input placeholder="Enquiry Title" class="fondo tituloPlace" type="text" name="title">
+                    <input placeholder="Coder Name"class="tituloPlace2 fondo" type="text" name="name">
+                    
+                    <div class="btn-group buttonGroup" role="group" aria-label="Basic example">
+
                         <button type="submit" class="btn btn-secondary send">Send</button>
-                        <a href="index.php">
-                        <button type="button" class="btn btn-secondary cancel">Cancel</button>
-                        </a>
+                        <button type="button" class="btn btn-secondary cancel"><a class="linkCancel" href="index.php">Cancel</a></button>
                     </div>
                 </div>
             </form>
         </div>
+    </div>
     </main>
 
 </body>
